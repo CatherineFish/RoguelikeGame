b1981404f9a35d6d725ee534d901b849dafb71c5
Andrey Larin <hakenlaken1@gmail.com>
2021-04-28 17:03:09 +0300

    Добавлена папка тестов из master

---
+++ tests/test_base.py
@@ -0,0 +1,27 @@
+import unittest
+import pygame_menu
+from base import Game, SCREEN_WIDTH, SCREEN_HEIGHT
+
+
+class TestBase(unittest.TestCase):
+    
+    def setUp(self):
+        self.game = Game()
+
+    def test_1_gameSize(self):      
+        self.assertEqual(self.game.screen.get_size(), (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    def test_2_menu(self):
+        self.assertEqual(vars(self.game.menu.menu)["_window_size"], (SCREEN_WIDTH, SCREEN_HEIGHT)) 
+        self.assertEqual(vars(vars(vars(self.game.menu.menu)["_theme"])["background_color"])["_filepath"], "intro.png") 
+        self.assertEqual(vars(vars(self.game.menu.menu)["_theme"])["title_background_color"], (76, 36, 25, 255))
+        self.assertEqual(vars(vars(self.game.menu.menu)["_theme"])["widget_padding"], 25)
+        self.assertEqual(vars(vars(self.game.menu.menu)["_theme"])["widget_font"], "Arial")
+        self.assertEqual(vars(vars(self.game.menu.menu)["_theme"])["title_font_shadow"], True)
+        self.assertEqual(vars(vars(self.game.menu.menu)["_theme"])["focus_background_color"], (217, 140, 63,255))
+        self.assertEqual(vars(vars(self.game.menu.menu)["_theme"])["selection_color"], (217, 178, 63, 255))
+        self.assertEqual(vars(vars(self.game.menu.menu)["_theme"])["title_font_color"], (217, 178, 63, 255))
+        self.assertEqual(vars(vars(self.game.menu.menu)["_theme"])["widget_font_color"], (217, 178, 63, 255))
+
+    def test_3_menu_obj(self):
+        self.assertEqual(len((vars(self.game.menu.menu)["_widget_columns"])[0]), 5) 
