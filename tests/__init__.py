619fbd25ab9f592fa7ff4996fccc49d1934111b4
Catherine Fish <kett_x@mail.ru>
2021-06-04 16:08:32 +0300

    Поправлен код в соответствии flake8 & pydocstyle

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,19 +1,25 @@
-import sys, os
+"""
+Тесты.
+
+Набор элементарных тестов.
+"""
+
+import sys
+import os
+import unittest
 testdir = os.path.dirname(__file__)
 srcdir = '../GameProject'
 sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
-
-import unittest
 from base import Game, SCREEN_WIDTH, SCREEN_HEIGHT, Player, Wall, Door, Exit, Floor, Dark, Coin, Trap, Enemy
-import os
 
 
 class TestBase(unittest.TestCase):
-    """Инициализация класса для тестирования"""
+    """Инициализация класса для тестирования."""
+
     mypath = 'GameProject/RoomsInDungeon/'
 
     def setUp(self):
-        """Инициализция нужных классов из base.py для последующей проверки"""
+        """Инициализция нужных классов из base.py для последующей проверки."""
         self.game = Game()
         self.player = Player(self.game, 0, 0)
         self.wall = Wall(self.game, 0, 0)
@@ -26,7 +32,7 @@ class TestBase(unittest.TestCase):
         self.coin = Coin(self.game, 0, 0)
 
     def test_1_room_objects(self):
-        """Проверка на то, нет ли в файлах комнат *.in неопознанных объектов"""
+        """Проверка на то, нет ли в файлах комнат *.in неопознанных объектов."""
         # print(os.getcwd())
         rooms_list = [f for f in os.listdir(
             self.mypath) if os.path.isfile(os.path.join(self.mypath, f))]
@@ -40,11 +46,11 @@ class TestBase(unittest.TestCase):
                 self.assertTrue(False)
 
     def test_2_gameSize(self):
-        """Проверка на то, совпадают ли размеры окна с имеющимися константами, заданными в base.py"""
+        """Проверка на то, совпадают ли размеры окна с имеющимися константами, заданными в base.py."""
         self.assertEqual(self.game.screen.get_size(), (SCREEN_WIDTH, SCREEN_HEIGHT))
 
     def test_3_menu(self):
-        """Проверка на то, корректно ли заданы параметры menu"""
+        """Проверка на то, корректно ли заданы параметры menu."""
         self.assertEqual(vars(self.game.menu.menu)["_window_size"], (SCREEN_WIDTH, SCREEN_HEIGHT))
         self.assertEqual(vars(vars(vars(self.game.menu.menu)["_theme"])[
                          "background_color"])["_filepath"], "GameProject/Screens/intro.png")
@@ -63,11 +69,11 @@ class TestBase(unittest.TestCase):
                          "widget_font_color"], (217, 178, 63, 255))
 
     def test_4_menu_obj(self):
-        """Проверка на то, корректно ли работают виджеты в menu"""
+        """Проверка на то, корректно ли работают виджеты в menu."""
         self.assertEqual(len((vars(self.game.menu.menu)["_widget_columns"])[0]), 5)
 
     def test_1_png_names(self):
-        """Проверка на то, все ли файлы *.png используемые для прорисовки скриптов выбранных классов имеются в репозитории"""
+        """Проверка на то, все ли файлы *.png используемые для прорисовки скриптов выбранных классов имеются в репозитории."""
         # print(os.getcwd())
         for name in self.game.png_names:
             self.assertTrue(os.path.exists(name))
